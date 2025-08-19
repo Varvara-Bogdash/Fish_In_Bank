7393b7dc7440fb2d57e97d864a0803f8db3e0e92
Test <varvara.bogdan10@gmail.com>
2025-08-19 15:03:13 +0300

    main

--- app/src/main/java/com/example/fishinbank/MainActivity.java
+++ app/src/main/java/com/example/fishinbank/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fishinbank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void add(View view) {
+
+    }
+
+    public void author(View view) {
+
+    }
+
+    public void purpose(View view) {
+
     }
 }

e55a0bb7d4a14da86e5a984ff4323d7e32ead9dd
Test <varvara.bogdan10@gmail.com>
2025-08-21 12:47:24 +0300

    add author screen

--- app/src/main/java/com/example/fishinbank/AuthorClass.java
+++ app/src/main/java/com/example/fishinbank/AuthorClass.java
@@ -6,24 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class AuthorClass extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_author_class);
     }
 
-    public void add(View view) {
-
-    }
-
-    public void author(View view) {
-        Intent intent = new Intent(this, AuthorClass.class);
+    public void home(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }
-
-    public void purpose(View view) {
-
     }
 }

b7dfc614354eea8d7cb48701ace7a16915a95639
Test <varvara.bogdan10@gmail.com>
2025-08-23 15:02:36 +0300

    change purpose screen

--- app/src/main/java/com/example/fishinbank/AddPurposeActivity.java
+++ app/src/main/java/com/example/fishinbank/AddPurposeActivity.java
@@ -2,7 +2,9 @@ package com.example.fishinbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddPurposeActivity extends AppCompatActivity {
 
@@ -12,4 +14,13 @@ public class AddPurposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_purpose);
     }
 
+    public void save(View view) {
+        Intent intent = new Intent(this, PurposeActivity.class);
+        startActivity(intent);
+    }
+
+    public void house(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
